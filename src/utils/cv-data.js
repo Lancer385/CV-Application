d45515c3c6a90fc4a0d9f28098981a7dfd098109
Lancer385 <ahmedkhalaf1234123@gmail.com>
2026-01-14 09:42:57 +0200

    add: data for components render

---
+++ src/utils/cv-data.js
@@ -0,0 +1,19 @@
+const cvData = {
+  generalInformation: {
+    fullName: "Sam Lee",
+    email: "sam.lee@test.com",
+    phoneNumber: "123-456-7890"
+  },
+
+  education: [
+    { schoolName: "Test University", titleOfStudy: "BSc CS", dateFrom: "2018", dateTo: "2022" }
+  ],
+
+  practicalExperience: [
+    { companyName: "TechCorp", positionTitle: "Dev", dateFrom: "2022", dateTo: "2023" },
+    { companyName: "WebSolutions", positionTitle: "Intern", dateFrom: "2021", dateTo: "2021" }
+  ]
+};
+
+
+export {cvData};
